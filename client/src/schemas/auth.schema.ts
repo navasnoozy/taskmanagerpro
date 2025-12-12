6fdc7c7f0d218792d290b50065896e17e43399da
navasnoozy <noozytechz@gmail.com>
2025-12-12 09:35:01 +0530

    validation for no email entered

--- client/src/schemas/auth.schema.ts
+++ client/src/schemas/auth.schema.ts
@@ -2,14 +2,24 @@
 import { z } from "zod/v4";
 
 export const signinSchema = z.object({
-  email: z.email("Invalid email address"),
+      email: z.email({
+      error: (issue) => {
+        if (!issue.input || issue.input === "") return "Email is required";
+        return "Invalid email address";
+      },
+    }),
   password: z.string().min(1, "Password is required"),
 });
 
 export const signupSchema = z
   .object({
     name: z.string().min(2, "Name must be at least 2 characters"),
-    email: z.email("Invalid email address"),
+    email: z.email({
+      error: (issue) => {
+        if (!issue.input || issue.input === "") return "Email is required";
+        return "Invalid email address";
+      },
+    }),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z.string().min(1, "Confirm password is required"),
   })
